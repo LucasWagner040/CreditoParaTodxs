d4f13571740a8afafb184b3594a0679e4820d534
Lucas wagner <lucaswagner040@gmail.com>
2021-06-19 00:59:11 -0300

    Salvando
    Finalizando a penúltima parte do projeto

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+
+function myFunction(val) {
+    var demo2 = val
+    var demo1 = document.getElementById("display").value;
+    var comJuros = parseInt(demo1) / parseInt(demo2) * 2;
+    document.getElementById("demo2").innerText = "R$ " + parseInt(comJuros)
+    document.getElementById("demo1").innerHTML = demo2 + "x"
+}
+function myFunction2(val) {
+    var demo1 = val
+    var demo2 = document.getElementById("display2").value;
+    var comJuros = parseInt(demo1) / parseInt(demo2) * 2;
+    document.getElementById("demo2").innerText = "R$ " + parseInt(comJuros)
+}
+function teste (){
+    var demo1 = document.getElementById("display").value;
+    var demo2 = document.getElementById("display2").value;
+    alert("---  EM CONSTRUÇÃO  ---")
+}
